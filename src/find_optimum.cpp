15734378d79a2db02e81e9839c866fa2d196ee9f
Charles Rocabert <charles.rocabert@gmail.com>
2025-07-11 16:40:34 +0200

    Update find_optimum.cpp

--- src/find_optimum.cpp
+++ src/find_optimum.cpp
@@ -36,8 +36,7 @@
 #include <sys/stat.h>
 #include <assert.h>
 
-#include "Config.h"
-//#include "../cmake/Config.h"
+#include "../cmake/Config.h"
 #include "./lib/Macros.hpp"
 #include "./lib/Enums.hpp"
 #include "./lib/Model.hpp"
